827ac5a21585ce46266a05660dccb8ed0fb51c0e
Boran <boran.u3@gmail.com>
2023-06-20 19:25:49 +0200

    Added 'created at' date in blog posts + background to home page

--- components/Post.vue
+++ components/Post.vue
@@ -1,18 +1,35 @@
 <script setup>
-const props = defineProps(['posts'])
+import moment from "moment";
+
+const props = defineProps(["posts"]);
+
+// Format date to be human readable
+const formatDate = (date) => {
+  return moment(date).format("MMMM Do YYYY");
+};
 </script>
 
 <template>
-  <div v-for="post in props.posts" :key="post.slug"
-    class="bg-white rounded shadow-md overflow-hidden hover:opacity-90">
+  <div
+    v-for="post in props.posts"
+    :key="post.slug"
+    class="bg-white rounded shadow-md overflow-hidden hover:opacity-90"
+  >
     <NuxtLink :to="post._path">
-      <img :src="`/images/blog/${post.cover}`" alt="Blog Post Cover Image" class="w-full h-48 object-cover">
+      <img
+        :src="`/images/blog/${post.cover}`"
+        alt="Blog Post Cover Image"
+        class="w-full h-48 object-cover"
+      />
     </NuxtLink>
     <div class="p-6">
       <h2 class="text-xl font-bold mb-2">{{ post.title }}</h2>
       <p class="text-gray-700 mb-4">{{ post.description }}</p>
-      <NuxtLink :to="post._path"
-        class="inline-block bg-indigo-800 hover:bg-indigo-600 text-white py-2 px-4 rounded float-right mb-8">Read More
+      <p class="text-gray-600 text-sm mb-2">{{ formatDate(post.date) }}</p>
+      <NuxtLink
+        :to="post._path"
+        class="inline-block bg-indigo-800 hover:bg-indigo-600 text-white py-2 px-4 rounded float-right mb-8"
+        >Read More
       </NuxtLink>
     </div>
   </div>
--- components/SiteHeader.vue
+++ components/SiteHeader.vue
@@ -23,7 +23,7 @@ const navigation = [
 </script>
 
 <template>
-  <Disclosure as="nav" class="bg-transparent shadow-md" v-slot="{ open }">
+  <Disclosure as="nav" class="bg-white shadow-md" v-slot="{ open }">
     <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
       <div class="relative flex h-16 items-center justify-between">
         <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
