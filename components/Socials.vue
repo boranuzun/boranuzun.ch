7c758777760822e25a47bef6d8cab5be64652385
Boran <boran.u3@gmail.com>
2023-06-21 19:15:45 +0200

    Added transition animation to Socials.vue

--- components/Socials.vue
+++ components/Socials.vue
@@ -24,7 +24,7 @@ const links = [
 </script>
 
 <template>
-  <a v-for="link in links" :key="link.name" :href="link.href">
+  <a v-for="link in links" :key="link.name" :href="link.href" class="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
     <Icon :name="link.icon" size="1.5rem" class="text-gray-800" />
   </a>
 </template>
