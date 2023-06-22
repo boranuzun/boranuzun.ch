961b43249ce940985f13cb3ecb50ef8e8cf525ae
Boran <boran.u3@gmail.com>
2023-06-22 18:46:44 +0200

    Added aria-label to social icons

--- components/Socials.vue
+++ components/Socials.vue
@@ -5,7 +5,7 @@ const links = [
     href: "mailto:boran.u3@gmail.com",
     icon: "material-symbols:mail",
   },
-    {
+  {
     name: "Mastodon",
     href: "https://mastodon.social/@boranuzun",
     icon: "fa6-brands:mastodon",
@@ -24,7 +24,17 @@ const links = [
 </script>
 
 <template>
-  <a v-for="link in links" :key="link.name" :href="link.href" class="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
-    <Icon :name="link.icon" size="1.5rem" class="text-gray-800" />
+  <a
+    v-for="link in links"
+    :key="link.name"
+    :href="link.href"
+    class="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
+  >
+    <Icon
+      :name="link.icon"
+      :aria-label="link.name"
+      size="1.5rem"
+      class="text-gray-800"
+    />
   </a>
 </template>
--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -4,13 +4,6 @@ const { path } = useRoute();
 const { data } = await useAsyncData(`content-${path}`, () => {
   return queryContent().where({ _path: path }).findOne();
 });
-
-useHead({
-  title: data.title,
-  meta: [
-    { name: 'description', content: data.description }
-  ],
-})
 </script>
 
 <template>
